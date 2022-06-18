e34de6af52996feddf5a278add2d626baf83d7ec
Dilman Arif <dilmanarif01@gmail.com>
2022-06-18 23:28:45 +0300

    Add Kurdistan Flag

--- lib/main.dart
+++ lib/main.dart
@@ -9,20 +9,31 @@ void main() {
 class HelloKurdistanApp extends StatelessWidget {
   const HelloKurdistanApp({Key? key}) : super(key: key);
 
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text(
-            'Hello Kurdistan!',
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-              color: Colors.red,
+          body: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Image.network(
+                  "https://upload.wikimedia.org/wikipedia/commons/1/17/Flag_of_Kurdistan_%28Khoiboun%29.png"),
             ),
-          ),
+            Text(
+              'Hello Kurdistan!',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.red,
+              ),
+            )
+          ],
         ),
-      ),
+      )),
     );
   }
 }
